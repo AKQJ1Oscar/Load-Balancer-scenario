c3af2fa190a50161f7acd4b445384138a1b6a826
Óscar Vázquez <oscarvb94@gmail.com>
2016-06-21 21:00:54 +0200

    versión 1.2

--- pfinalp1.py
+++ pfinalp1.py
@@ -6,7 +6,7 @@ from lxml import etree
 
 # Comprobación de parámetros pasados al script
 if (len(sys.argv) != 2 and len(sys.argv) != 3) or sys.argv[1] not in ["-create", "-start", "-stop", "-destroy", "-monitor"]:
-	print "Uso del script: python pfinalp1.py [comando] [opciones]"
+	print chr(27) + "[0;44m" + "Uso del script: python pfinalp1.py [comando] [opciones]" + chr(27) + "[0m"
 	print "Lista de comandos:"
 	print "	-create		--> Crea el escenario"
 	print "	-start n	--> Arranca n servidores (n entre 1 y 5)"
@@ -16,7 +16,7 @@ if (len(sys.argv) != 2 and len(sys.argv) != 3) or sys.argv[1] not in ["-create",
 	sys.exit()
 elif len(sys.argv) == 3:
 	if sys.argv[1] != '-start' and sys.argv[1] != '-monitor':
-		print "El comando que ha introducido no utiliza opciones"
+		print chr(27) + "[0;41m" + "El comando que ha introducido no utiliza opciones" + chr(27) + "[0m"
 		sys.exit()
 	
 # Parámetro que define la operación a realizar
@@ -28,6 +28,8 @@ mv = ["c1", "lb", "s1", "s2", "s3", "s4", "s5"]
 nserver_max = len(mv) - 2
 
 if orden == '-create':
+	print chr(27) + "[0;44m" + "Creando el escenario..." + chr(27) + "[0m"
+
 	# Creamos los ficheros .qcow2 de diferencias
 	for i in range (0, len(mv)):
 		os.system("qemu-img create -f qcow2 -b cdps-vm-base-p3.qcow2 " + mv[i] + ".qcow2")
@@ -71,7 +73,7 @@ if orden == '-create':
 	os.system("sudo ifconfig LAN1 up")
 	os.system("sudo ifconfig LAN2 up")
 
-	print "Se ha creado el escenario"
+	print chr(27) + "[0;42m" + "Se ha creado el escenario" + chr(27) + "[0m"
 
 elif orden == '-start':
 	# Si existe c1.xml es porque se ha creado el escenario
@@ -85,20 +87,21 @@ elif orden == '-start':
 			server_config.close()
 			# Si el número de servidores a arrancar está entre 1 y 5
 			if nserver > 0 and nserver <= nserver_max:
+				print chr(27) + "[0;44m" + "Arrancando el escenario..." + chr(27) + "[0m"
 				# Para c1, lb y cada uno de los servidores elegidos
 				for i in range (0, nserver + 2):
 					# Arrancamos su MV y mostramos su consola
 					if subprocess.call(['sudo', 'virsh', 'create', mv[i] + '.xml']) != 0:
 						os.system("sudo virsh create " + mv[i] + ".xml")
 					os.system("sudo virt-viewer " + mv[i] + "&")
-				time.sleep(0.1)
-				print "Se ha arrancado el escenario con %d servidores" % nserver
+				time.sleep(0.25)
+				print chr(27) + "[0;42m" + "Se ha arrancado el escenario con %d servidores" % nserver + chr(27) + "[0m"
 			else:
-				print "El número de servidores a arrancar debe estar entre 1 y %d" % nserver_max
+				print chr(27) + "[0;41m" + "El número de servidores a arrancar debe estar entre 1 y %d" % nserver_max + chr(27) + "[0m"
 		else:
-			print "Debe indicar el número de servidores a arrancar con un número entre 1 y %d" % nserver_max
+			print chr(27) + "[0;41m" + "Debe indicar el número de servidores a arrancar con un número entre 1 y %d" % nserver_max + chr(27) + "[0m"
 	else:
-		print "Para poder arrancar servidores primero debe crear el escenario"
+		print chr(27) + "[0;41m" + "Para poder arrancar servidores primero debe crear el escenario" + chr(27) + "[0m"
 
 elif orden == '-stop':
 	# Si existe .server_config.txt es que se han arrancado previamente MVs
@@ -107,12 +110,14 @@ elif orden == '-stop':
 		server_config = open('.server_config.txt', 'r')
 		nserver_ = server_config.read()
 		nserver = int(nserver_)
+		print chr(27) + "[0;44m" + "Deteniendo el escenario..." + chr(27) + "[0m"
 		# Paramos las MV que se habían arrancado previamente
 		for i in range (0, nserver + 2):
 			os.system("sudo virsh shutdown " + mv[i])	
-		print "Se han detenido las MV"
+		time.sleep(0.25)
+		print chr(27) + "[0;42m" + "Se han detenido las MV del escenario" + chr(27) + "[0m"
 	else:
-		print "No se pueden detener MVs ya que ninguna ha sido arrancada"
+		print chr(27) + "[0;41m" + "No se pueden detener MVs ya que ninguna ha sido arrancada" + chr(27) + "[0m"
 
 elif orden == '-destroy':
 	# Si existe .server_config.txt es que se han arrancado previamente MVs
@@ -121,6 +126,7 @@ elif orden == '-destroy':
 		server_config = open('.server_config.txt', 'r')
 		nserver_ = server_config.read()
 		nserver = int(nserver_)
+		print chr(27) + "[0;44m" + "Destruyendo y liberando el escenario..." + chr(27) + "[0m"
 		# Destroy de las MV arrancadas
 		for i in range (0, nserver + 2):
 			os.system("sudo virsh destroy " + mv[i])
@@ -138,14 +144,14 @@ elif orden == '-destroy':
 		os.system("sudo ifconfig LAN2 down")	
 		os.system("sudo brctl delbr LAN1")
 		os.system("sudo brctl delbr LAN2")
-		print "Se ha liberado el escenario"
+		print chr(27) + "[0;42m" + "Se ha destruido y liberado el escenario" + chr(27) + "[0m"
 	else:
-		print "El escenario no ha sido creado"
+		print chr(27) + "[0;41m" + "El escenario no ha sido creado" + chr(27) + "[0m"
 
 elif orden == '-monitor':
 	if len(sys.argv) == 2:
-		print "A continuación se muestra el estado de todas las MVs arrancadas"
-		print "Si quiere información sobre una MV en concreto, introduzca su nombre tras el comando monitor"
+		print chr(27) + "[0;44m" + "Estado de las MVs arrancadas" + chr(27) + "[0m"
+		print "Para información sobre una MV en concreto, introduzca su nombre tras el comando monitor"
 		print ""
 		# Listamos las MV que se encuentran funcionando
 		os.system("sudo virsh list --all")
@@ -161,14 +167,12 @@ elif orden == '-monitor':
 				if sys.argv[2] == mv[i]:
 					mvname = sys.argv[2]
 			if mvname == '':
-				print "Introduzca el nombre de una MV que haya sido arrancada"
+				print chr(27) + "[0;41m" +  "Introduzca el nombre de una MV que haya sido arrancada" + chr(27) + "[0m"
 				sys.exit()			
 			else:
-				print "Información de %s:" % mvname
+				print chr(27) + "[0;44m" + "Información de %s:" % mvname + chr(27) + "[0m"
 				os.system("sudo virsh dominfo %s" % mvname)			
-				#print ""
-				print "Estado de %s:" % mvname
+				print chr(27) + "[0;44m" + "Estado de %s:" % mvname + chr(27) + "[0m"
 				os.system("sudo virsh domstate %s" % mvname)
-				#print ""
-				print "CPU stats de %s:" % mvname
+				print chr(27) + "[0;44m" + "CPU stats de %s:" % mvname + chr(27) + "[0m"
 				os.system("sudo virsh cpu-stats %s" % mvname)
